92daecf9d7ddf529634280b58db8e82eded67e9a
KumarSunil17 <42208638+KumarSunil17@users.noreply.github.com>
2020-04-02 17:20:42 +0530

    home progress fix

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -72,7 +72,6 @@ class _HomePageState extends State<HomePage>
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return WillPopScope(
       onWillPop: () {
         if (_controller.isDismissed) {
